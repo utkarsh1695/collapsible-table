e1f5997dafaac8975b39dbf04a4793843901096f
Utkarsh Bhatia <utkarsh.bhatia@paytm.com>
2020-04-04 16:42:01 +0530

    Updates

---
+++ precache-manifest.55560d2ff34acbfd82c44c96f919827c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0441a20ea60e40ad3c3d189922f4c6",
+    "url": "/collapsible-table/index.html"
+  },
+  {
+    "revision": "2345713bf280577dd0a4",
+    "url": "/collapsible-table/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "07430a12c2bcdffda0dd",
+    "url": "/collapsible-table/static/js/2.0aab7a3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/collapsible-table/static/js/2.0aab7a3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2345713bf280577dd0a4",
+    "url": "/collapsible-table/static/js/main.32dbed9e.chunk.js"
+  },
+  {
+    "revision": "8bd300c07938b65d31a8",
+    "url": "/collapsible-table/static/js/runtime-main.27f80c2e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/collapsible-table/static/media/logo.5d5d9eef.svg"
+  }
+]);
